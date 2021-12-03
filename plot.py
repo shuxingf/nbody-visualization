b55296b4573bd04fc6f5837400e027f07ea605df
shuxingf <shuxingf@usc.edu>
2021-12-02 22:09:28 -0800

    test

--- plot.py
+++ plot.py
@@ -88,7 +88,7 @@ def plot_snapshot(snapshot, f_short_cdm,  f_short_sidm,  LMC_main, LMC_main_vi, 
 
         plt.subplot(111)
        
-        plt.set_facecolor('black')
+        
         plt.scatter(f_short_cdm[snapshot[i]]['pos'][:,0],
                 f_short_cdm[snapshot[i]]['pos'][:,1],s=0.01,alpha=0.1,c='dodgerblue')
         
@@ -129,11 +129,11 @@ def plot_snapshot(snapshot, f_short_cdm,  f_short_sidm,  LMC_main, LMC_main_vi, 
 
         plt.figure(figsize=(12,12))
 
-        plt.subplot(111)
+        plt.subplot(111, facecolor = ('black'))
        
         plt.scatter(f_short_sidm[snapshot[i]]['pos'][:,0],
             f_short_sidm[snapshot[i]]['pos'][:,1],s=0.01,alpha=0.1,c='k')
-        plt.set_facecolor('black')
+        
         
         #try:
         #    mw = plt.scatter(MW_main_vi[index]['x'], MW_main_vi[index]['y'], 
@@ -153,7 +153,7 @@ def plot_snapshot(snapshot, f_short_cdm,  f_short_sidm,  LMC_main, LMC_main_vi, 
         plt.legend(loc=2,handles=[lmc],fontsize=20,frameon=False)
        
 
-        plt.axis('off')
+        plt.axis('on')
 
         plt.tight_layout()
         if(param == "MW"):
