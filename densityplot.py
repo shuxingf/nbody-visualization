db4d1a8043c9afd77861fa9dc78cf5c1686e7130
shuxingf <shuxingf@usc.edu>
2021-12-07 16:27:49 -0800

    .

--- densityplot.py
+++ densityplot.py
@@ -3,6 +3,7 @@ import matplotlib.pylab as plt
 import matplotlib.cm as cm
 import matplotlib.colors as colors
 from matplotlib.colors import LogNorm
+import math
 
 #see https://github.com/pynbody/pynbody for installation
 import pynbody
@@ -111,9 +112,15 @@ def plot(snapshot, f_short_cdm,  f_short_sidm,  LMC_main, LMC_main_vi, f_cdm, f_
         f_short_cdm[key] = f_cdm[key][(dist<distance_cut) & (np.abs(zdist)<projection_thickness)]
 
 
-    for i,r in enumerate(r_bins):
-        for key in f_short_cdm.keys():
+
+    for key in f_short_cdm.keys():
+        for i,r in enumerate(r_bins):
             particles = len(f_short_cdm[key][dist[key] < r])
+            density = particles/(4/3 * math.pi() * r^2)
+            rho_enclosed[i] = particles/density
+        plt.loglog(r_bins,rho_enclosed)
+        plt.savefig("/home1/shuxingf/nbody-visualization/density/cdm_density" + key + ".png")
+
             
   #solar masses/ 
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subselect2
 import load
 import plot 
 import sys
+import densityplot
 
 
 param = sys.argv[1]
@@ -20,10 +21,13 @@ LMC_main, LMC_main_vi, MW_main, MW_main_vi = load.load(snapshot,f_cdm, cdm_halos
 #subselect
 f_short_cdm = {}
 f_short_sidm = {}
+if(param == "D"):
+    densityplot.plot(snapshot, f_short_cdm,  f_short_sidm,  LMC_main, LMC_main_vi, f_cdm, f_sidm)
+    
 if(param == "MW"):
     subselect.subselect(cdm_halos, sidm_halos, f_cdm, f_short_cdm, f_sidm, f_short_sidm)
 if(param == "LMC"):
     subselect2.subselect(f_cdm, f_short_cdm, f_sidm, f_short_sidm ,LMC_main, LMC_main_vi)
 
 #plot
-plot.plot_snapshot(snapshot, f_short_cdm, f_short_sidm, LMC_main, LMC_main_vi, MW_main, MW_main_vi, param)
+#plot.plot_snapshot(snapshot, f_short_cdm, f_short_sidm, LMC_main, LMC_main_vi, MW_main, MW_main_vi, param)
